c17f41e6d8e40c81e3c205866a69d7a8bcbeb303
dogrob96 <dogrob96@gmail.com>
2015-05-22 20:27:58 +1000

    Dev #6

---
+++ ConquestPlugin/ChatHandlers/HandleGetFP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ConquestPlugin.GameModes;
+using ConquestPlugin.Utility;
+namespace ConquestPlugin.ChatHandlers
+{
+    class HandleGetFP : ChatHandlerBase
+    {
+        public override string GetHelp()
+        {
+            return "Usage /GetFP";
+        }
+        public override string GetCommandText()
+        {
+            return "/GetFP";
+        }
+        public override bool IsAdminCommand()
+        {
+            return false;
+        }
+        public override bool AllowedInConsole()
+        {
+            return true;
+        }
+        public override bool IsClientOnly()
+        {
+            return true;
+        }
+        public override bool HandleCommand(ulong userId, string[] words)
+        {
+       //    ChatUtil.SendPrivateChat(userId, FactionPoints.getFP(Faction.getFactionID(userId)));
+            return true;
+        }
+    }
+}
