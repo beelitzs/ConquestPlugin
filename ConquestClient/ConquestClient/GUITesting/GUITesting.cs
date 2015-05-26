870c2c5d7d482ccd3c6ae53afae8435e3883a555
dogrob96 <dogrob96@gmail.com>
2015-05-26 12:37:53 +1000

    client guitest

---
+++ ConquestClient/ConquestClient/GUITesting/GUITesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+using Sandbox.Game.Gui;
+using Sandbox.Game.GUI;
+using Sandbox.Graphics.GUI;
+using Sandbox.ModAPI;
+using SEModAPI;
+using SEModAPIExtensions;
+
+namespace ConquestClient.GUITesting
+{
+    class GUITesting
+    {
+        public static void guitest()
+        {
+            
+        }
+    }
+}
