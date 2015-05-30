61819842e655a77e0807f81de5390ab8e109cd67
dogrob96 <dogrob96@gmail.com>
2015-05-30 17:28:03 +1000

    more bug fixes

--- ConquestPlugin/ChatHandlers/ShopHandles/HandleShop.cs
+++ ConquestPlugin/ChatHandlers/ShopHandles/HandleShop.cs
@@ -5,6 +5,8 @@ using ConquestPlugin.Utility;
 using ConquestPlugin.Utility.Shop;
 using Sandbox.Common;
 using Sandbox.ModAPI;
+using SEModAPIExtensions.API;
+using SEModAPIInternal.API.Common;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Common.ObjectBuilders.Definitions;
 
@@ -40,20 +42,20 @@ namespace ConquestPlugin.ChatHandlers
         public override bool HandleCommand(ulong userId, string[] words)
         {
             MyObjectBuilder_Faction currentfaction;
-            currentfaction = Faction.getFaction(Faction.getFactionID(userId));
+            currentfaction = Utility.Faction.getFaction(Utility.Faction.getFactionID(userId));
             foreach (MyObjectBuilder_FactionMember currentmember in currentfaction.Members)
             {
-                if (currentmember.IsLeader == true)//currentmember.isleader(currentfaction)
+                if (currentmember.IsLeader == true && currentmember.PlayerId == PlayerMap.Instance.GetPlayerIdsFromSteamId(userId).First())//currentmember.isleader(currentfaction)
                 {
                     string output = "";
                     output = Utility.Shop.Shop.getShopList(userId);
                     ChatUtil.DisplayDialog(userId, "Faction Store", "Spend FP Here!", output);
                     break;
                 }
-                else
+                else if (currentmember.PlayerId == PlayerMap.Instance.GetPlayerIdsFromSteamId(userId).First())
                 {
                     ChatUtil.SendPrivateChat(userId,"You do not have Permission to use this command.");
-                    break;
+                   
                 }
             }
             return true;
